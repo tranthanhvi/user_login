69f534a694f5cbe805965942b27eab5501f2226f
Thanh Tran <tranthanhvi66@gmail.com>
2024-05-09 19:27:43 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        filename: 'bundle.js' // Output file name
+    }
+};
